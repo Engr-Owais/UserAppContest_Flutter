bb8640d1be567f8a5491ab8c26f6d06995aec915
Ismailtalha <ismailtalha789@gmil.com>
2020-10-10 20:11:55 +0500

    splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:contest_user_app/Screens/OnGoingContest.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-
+import 'package:google_fonts/google_fonts.dart';
+import 'package:splashscreen/splashscreen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,14 +15,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-       
-        primarySwatch: Colors.amber,
-       
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home:OnGoingContest()
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'CONTEST',
+        theme: ThemeData(
+          primarySwatch: Colors.amber,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: new SplashScreen(
+            seconds: 4,
+            navigateAfterSeconds: OnGoingContest(),
+            title: new Text('Welcome To Contestor',
+                style: GoogleFonts.abel(
+                    fontWeight: FontWeight.bold, fontSize: 20)),
+            image: new Image(
+              image: AssetImage("assets/buffersplash.png"),
+            ),
+            backgroundColor: Colors.amber,
+            styleTextUnderTheLoader: new TextStyle(),
+            photoSize: 100,
+            loaderColor: Colors.red));
   }
 }
